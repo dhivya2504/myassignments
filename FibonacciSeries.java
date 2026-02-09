f489a95a22f4fad4b5cef5af18c39e258d964925
dhivya2504 <dhivya2504@gmail.com>
2026-02-09 19:19:15 +0530

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,20 @@
+package week2.day1;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int Total=9;
+		int i=0;
+		int j=1;
+		for(int k = 1;k<Total;++k) {
+		System.out.println(i+"");
+		int series=i+j;
+		i=j;
+		j=series;
+		
+		}
+
+	}
+
+}
