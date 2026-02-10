97d23ad28efde9a554bb662aff7a62b1166463fa
dhivya2504 <dhivya2504@gmail.com>
2026-02-10 09:38:51 +0530

    Add files via upload

---
+++ week2/day1/Isprime.java
@@ -0,0 +1,25 @@
+package week2.day1;
+
+public class Isprime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            int a=5;
+          
+    		
+            for(int i=2; i<a; i++) 
+			{
+				if (a%i==0)
+				{
+				
+					System.out.println(a + " is a not a Prime Number");
+					 
+				}
+				else {
+					System.out.println(a + " is a Prime Number");
+				}
+			}
+            }
+	}
+
+
