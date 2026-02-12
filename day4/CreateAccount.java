b70672aafaf0466ce71e0ae2c2aa5634f6de0b30
dhivya2504 <dhivya2504@gmail.com>
2026-02-12 19:34:19 +0530

    Add files via upload

---
+++ day4/CreateAccount.java
@@ -0,0 +1,25 @@
+package week2.day4;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CreateAccount {
+
+	public static void main(String[] args) {
+		ChromeDriver driver = new ChromeDriver();
+		driver.get(" http://leaftaps.com/opentaps/");
+		driver.manage().window().maximize(); 
+		driver.findElement(By.id("username")).sendKeys("DemoCSR2");
+	    driver.findElement(By.id("password")).sendKeys("crmsfa");
+	    driver.findElement(By.className("decorativeSubmit")).click();
+	    driver.findElement(By.id("label")).click();
+	    driver.findElement(By.linkText("Accounts")).click();
+	    driver.findElement(By.linkText("Create Account")).click();
+	    driver.findElement(By.id("accountName")).sendKeys("dhivya");
+	    driver.findElement(By.name("description")).sendKeys("Selenium Automation Tester");
+	    driver.findElement(By.id("officeSiteName")).sendKeys("“LeafTaps");
+	    driver.findElement(By.className("smallSubmit")).click();
+
+	    driver.close();
+	}
+}
